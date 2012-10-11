dd767093f7415ef1f7e4e4bf85db8242fc393527
Adam Temple <adstemple@comcast.net>
2012-10-10 20:13:07 -0600

    Issue #23: Added a few platforms to level 1

--- src/LevelEditor/LevelCreator.cs
+++ src/LevelEditor/LevelCreator.cs
@@ -15,26 +15,60 @@ namespace LevelEditor
             LevelData levelData = new LevelData();
             levelData.levelName = "level" + level;
 
+            // 1 platform
             SpriteData newSprite = new SpriteData();
             newSprite.position = new Vector2(1000, 400);
             newSprite.repeat = false;
             levelData.sprites.Add(newSprite);
 
+            // 2 platform
             newSprite = new SpriteData();
             newSprite.position = new Vector2(1100, 320);
             newSprite.repeat = false;
             levelData.sprites.Add(newSprite);
 
+            // 3 platform
             newSprite = new SpriteData();
             newSprite.position = new Vector2(1250, 320);
             newSprite.repeat = false;
             levelData.sprites.Add(newSprite);
 
+            // 4 platform
+            newSprite = new SpriteData();
+            newSprite.position = new Vector2(1350, 240);
+            newSprite.repeat = false;
+            levelData.sprites.Add(newSprite);
+
+            // 5 platform
+            newSprite = new SpriteData();
+            newSprite.position = new Vector2(1450, 160);
+            newSprite.repeat = false;
+            levelData.sprites.Add(newSprite);
+
+            // 6 platform
+            newSprite = new SpriteData();
+            newSprite.position = new Vector2(1600, 220);
+            newSprite.repeat = false;
+            levelData.sprites.Add(newSprite);
+
+            // 7 platform
+            newSprite = new SpriteData();
+            newSprite.position = new Vector2(1750, 280);
+            newSprite.repeat = false;
+            levelData.sprites.Add(newSprite);
+
+            // 8 platform
+            newSprite = new SpriteData();
+            newSprite.position = new Vector2(1900, 340);
+            newSprite.repeat = false;
+            levelData.sprites.Add(newSprite);
+
+            // 1 pinwheel
             newSprite = new SpriteData();
             newSprite.obstacleType = collisionType.OBSTACLE;
             newSprite.textureName = "pin wheel";
             newSprite.scale = 0.4f;
-            newSprite.position = new Vector2(1700, 350);
+            newSprite.position = new Vector2(2080, 350);
             newSprite.rotationSpeed = 0.02f;
             newSprite.repeat = false;
             levelData.sprites.Add(newSprite);
@@ -43,7 +77,7 @@ namespace LevelEditor
             newSprite.obstacleType = collisionType.OBSTACLE;
             newSprite.textureName = "pin";
             newSprite.scale = 0.4f;
-            newSprite.position = new Vector2(1700, 350);
+            newSprite.position = new Vector2(2080, 350);
             newSprite.repeat = false;
             levelData.sprites.Add(newSprite);
 
