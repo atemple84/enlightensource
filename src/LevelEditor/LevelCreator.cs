5388b00803954596402152f0bb63baf9534a3dbf
Adam Temple <adstemple@comcast.net>
2012-10-20 21:00:09 -0600

    Issue #35: Add a few backgrounds to level 1

--- src/LevelEditor/LevelCreator.cs
+++ src/LevelEditor/LevelCreator.cs
@@ -15,8 +15,100 @@ namespace LevelEditor
             LevelData levelData = new LevelData();
             levelData.levelName = "level" + level;
 
-            // 1 platform
+            // 1 background hill
             SpriteData newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(100, 500);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 2 background hill
+            newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(250, 400);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 3 background hill
+            newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(400, 550);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 4 background hill
+            newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(550, 300);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 5 background hill
+            newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(700, 450);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 6 background hill
+            newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(850, 400);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 7 background hill
+            newSprite = new SpriteData();
+            newSprite.textureName = "hill back";
+            newSprite.position = new Vector2(1000, 550);
+            newSprite.scale = 0.5f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(50, 0);
+            levelData.sprites.Add(newSprite);
+
+            // Sun
+            newSprite = new SpriteData();
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.textureName = "sun";
+            newSprite.scale = 0.35f;
+            newSprite.color = Color.White;
+            newSprite.position = new Vector2(800, 85);
+            newSprite.gameSpeed = new Vector2(35, 0);
+            levelData.sprites.Add(newSprite);
+
+            // Sun glow
+            newSprite = new SpriteData();
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.textureName = "sun glow";
+            newSprite.scale = 0.35f;
+            newSprite.color = Color.White;
+            newSprite.position = new Vector2(800, 85);
+            newSprite.gameSpeed = new Vector2(35, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 1 rainbow
+            newSprite = new SpriteData();
+            newSprite.textureName = "rainbow";
+            newSprite.position = new Vector2(400, 200);
+            newSprite.scale = 0.65f;
+            newSprite.obstacleType = collisionType.BACKGROUND;
+            newSprite.gameSpeed = new Vector2(0, 0);
+            levelData.sprites.Add(newSprite);
+
+            // 1 platform
+            newSprite = new SpriteData();
             newSprite.position = new Vector2(1000, 400);
             newSprite.repeat = false;
             levelData.sprites.Add(newSprite);
